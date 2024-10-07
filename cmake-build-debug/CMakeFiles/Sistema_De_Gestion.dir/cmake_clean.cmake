aec95a3554fef3bd845eb1bafdd25c9e5749b2ce
Lautaro Camejo <lautarocamejo@gmail.com>
2024-10-07 16:04:05 -0300

    Creacion Del Proyecto
    Elegimos el IDE CLion para el desarrollo del sistema de gestion, commit con la estructura basica.

---
+++ cmake-build-debug/CMakeFiles/Sistema_De_Gestion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sistema_De_Gestion.dir/src/main.cpp.obj"
+  "CMakeFiles/Sistema_De_Gestion.dir/src/main.cpp.obj.d"
+  "Sistema_De_Gestion.exe"
+  "Sistema_De_Gestion.exe.manifest"
+  "Sistema_De_Gestion.pdb"
+  "libSistema_De_Gestion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sistema_De_Gestion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Sistema_De_Gestion.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sistema_De_Gestion.
+# This may be replaced when dependencies are built.
